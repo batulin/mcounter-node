23ad2fda4e2b8e25f389ce456b260874d76daff2
evgen <batulin-1983@mail.ru>
2023-12-20 21:42:53 +0600

    1

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const usercontroller = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/registration', usercontroller.registration)
 router.post('/login', usercontroller.login)
-router.get('/auth', usercontroller.check)
+router.get('/auth', authMiddleware, usercontroller.check)
 
 module.exports = router
